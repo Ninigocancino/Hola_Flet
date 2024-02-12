4d093423a09a3dddfb27bd497d647407432764db
VictorCancino <juanvictorcancino@gmail.com>
2024-02-12 11:21:13 -0600

    Se agregó una función vacia en el fichero 'pass.py' para mostrar la estructura básica de una app en flet

---
+++ contador/pass.py
@@ -0,0 +1,9 @@
+#Veremos la estructura básica de una aplicación en Flet por lo que usaremos una función con un valor pass que disparará un elemento "control de carga/actualizar" al ejecutar el código
+
+import flet as ft 
+
+def main(page: ft.Page):
+
+    pass
+
+ft.app(target=main)
