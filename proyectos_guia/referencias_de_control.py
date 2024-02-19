650d623d60f29bc8e632cd29003c2115551cade6
VictorCancino <juanvictorcancino@gmail.com>
2024-02-19 10:42:41 -0600

    Se reescribió el programa en el fichero 'refrenecias_de_control.py' para usar referencias en el mismo. El código del programa anterior no se elimina, simplemente se comenta para poder hacer comparación entre ambas formas de escribir código para Flet

--- proyectos_guia/referencias_de_control.py
+++ proyectos_guia/referencias_de_control.py
@@ -1,5 +1,8 @@
 import flet as ft
 
+#Forma habitual con la que podríamos crear un formulario:
+
+"""
 def main(page):
 
     nombre = ft.TextField(label="Nombre", autofocus=True)
@@ -18,6 +21,28 @@ def main(page):
         apellido,
         ft.ElevatedButton("Di hola!", on_click=btn_click),
         saludo,
+    )
+"""
+
+#En esta forma reescribiremos el programa para usar referencias:
+def main(page):
+
+    nombre = ft.Ref[ft.TextField]()
+    apellido = ft.Ref[ft.TextField]()
+    saludo = ft.Ref[ft.Column]()
+
+    def btn_click(e):
+        saludo.current.controls.append(ft.Text(f"Hola, {nombre.value} {apellido.value}!"))
+        nombre.value = ""
+        apellido.value = ""
+        page.update()
+        nombre.focus()
+
+    page.add(
+        ft.TextField(ref=nombre, label="Nombre", autofocus=True),
+        ft.TextField(ref=apellido,label="Apellido"),
+        ft.ElevatedButton("Dí hola,hola", on_click=btn_click),
+        ft.Column(ref=saludo),
     )
 
 ft.app(target=main)
