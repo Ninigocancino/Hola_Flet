5f0398fa72b33dc1f1283a59de82a841d8e62ec9
VictorCancino <juanvictorcancino@gmail.com>
2024-03-22 22:41:17 -0600

    Definimos la función main y agregamós la lineas que pintan los botones de la claculadora

---
+++ calculadora/main.py
@@ -0,0 +1,30 @@
+import flet as ft
+
+def main(page: ft.page):
+    page.title = "Calculadora app"
+    resultado = ft.Text(value="0")
+
+    page.add(
+        resultado,
+        ft.ElevatedButton(text="AC"), #Pinta un botón con el string 'AC'
+        ft.ElevatedButton(text="+/-"),
+        ft.ElevatedButton(text="%"),
+        ft.ElevatedButton(text="/"),
+        ft.ElevatedButton(text="7"),
+        ft.ElevatedButton(text="8"),
+        ft.ElevatedButton(text="9"),
+        ft.ElevatedButton(text="*"),
+        ft.ElevatedButton(text="4"),
+        ft.ElevatedButton(text="5"),
+        ft.ElevatedButton(text="6"),
+        ft.ElevatedButton(text="-"),
+        ft.ElevatedButton(text="1"),
+        ft.ElevatedButton(text="2"),
+        ft.ElevatedButton(text="3"),
+        ft.ElevatedButton(text="+"),
+        ft.ElevatedButton(text="0"),
+        ft.ElevatedButton(text="."),
+        ft.ElevatedButton(text="="),
+    )
+
+ft.app(target=main)
