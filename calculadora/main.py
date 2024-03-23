057a9dacb11e71c120ba3691db24c377a4776a9e
VictorCancino <juanvictorcancino@gmail.com>
2024-03-22 22:59:33 -0600

    Organizamos los botones en filas para dar simular la dispoción de los botones en una calculadora

--- calculadora/main.py
+++ calculadora/main.py
@@ -2,29 +2,53 @@ import flet as ft
 
 def main(page: ft.page):
     page.title = "Calculadora app"
-    resultado = ft.Text(value="0")
+    resultado = ft.Text(value="0") #Guarda el valor del resultado por default
 
     page.add(
-        resultado,
-        ft.ElevatedButton(text="AC"), #Pinta un botón con el string 'AC'
-        ft.ElevatedButton(text="+/-"),
-        ft.ElevatedButton(text="%"),
-        ft.ElevatedButton(text="/"),
-        ft.ElevatedButton(text="7"),
-        ft.ElevatedButton(text="8"),
-        ft.ElevatedButton(text="9"),
-        ft.ElevatedButton(text="*"),
-        ft.ElevatedButton(text="4"),
-        ft.ElevatedButton(text="5"),
-        ft.ElevatedButton(text="6"),
-        ft.ElevatedButton(text="-"),
-        ft.ElevatedButton(text="1"),
-        ft.ElevatedButton(text="2"),
-        ft.ElevatedButton(text="3"),
-        ft.ElevatedButton(text="+"),
-        ft.ElevatedButton(text="0"),
-        ft.ElevatedButton(text="."),
-        ft.ElevatedButton(text="="),
+        ft.Row(controls=[resultado]), #Muestra el resultado en pantlla
+        ft.Row(
+            controls =[
+                ft.ElevatedButton(text="AC"), #Pinta un botón con el string 'AC'
+                ft.ElevatedButton(text="+/-"),
+                ft.ElevatedButton(text="%"),
+                ft.ElevatedButton(text="/"),
+            ]
+        ),
+        
+        ft.Row(
+            controls=[
+                ft.ElevatedButton(text="7"),
+                ft.ElevatedButton(text="8"),
+                ft.ElevatedButton(text="9"),
+                ft.ElevatedButton(text="*"),
+            ]
+        ),
+
+        ft.Row(
+            controls=[
+                ft.ElevatedButton(text="4"),
+                ft.ElevatedButton(text="5"),
+                ft.ElevatedButton(text="6"),
+                ft.ElevatedButton(text="-"),
+            ]
+        ),
+
+        ft.Row(
+            controls=[
+                ft.ElevatedButton(text="1"),
+                ft.ElevatedButton(text="2"),
+                ft.ElevatedButton(text="3"),
+                ft.ElevatedButton(text="+"),             
+            ]
+        ),
+
+        ft.Row(
+            controls=[
+                ft.ElevatedButton(text="0"),
+                ft.ElevatedButton(text="."),
+                ft.ElevatedButton(text="="),   
+            ]
+        ),
     )
 
 ft.app(target=main)
