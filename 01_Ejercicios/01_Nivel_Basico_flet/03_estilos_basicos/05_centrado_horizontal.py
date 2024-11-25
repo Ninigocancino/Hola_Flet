866f18f1090ed614008d0d8e21601883f2147b5d
VictorCancino <juanvictorcancino@gmail.com>
2024-11-24 19:51:26 -0600

    Se completó el ejercicio 'centrar texto'

--- 01_Ejercicios/01_Nivel_Basico_flet/03_estilos_basicos/05_centrado_horizontal.py
+++ 01_Ejercicios/01_Nivel_Basico_flet/03_estilos_basicos/05_centrado_horizontal.py
@@ -3,9 +3,11 @@ import flet as ft
 def main(page: ft.Page):
 
     page.title="Alinear texto al centro"
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
-    linea = ft.Text("Este texto se alinea al centro")
 
-    page.add(linea)
+    texto_centrado = ft.Text("Este texto se alinea al centro")
+
+    page.add(texto_centrado)
 
 ft.app(target=main)
