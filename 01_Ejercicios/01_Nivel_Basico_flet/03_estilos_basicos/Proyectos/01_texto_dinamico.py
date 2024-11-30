3f31b806670a7bb8f37e3419bd6af7f82f464fb5
VictorCancino <juanvictorcancino@gmail.com>
2024-11-30 15:15:58 -0600

    Se agregarón mejoras al prooyector integrador 01

--- 01_Ejercicios/01_Nivel_Basico_flet/03_estilos_basicos/Proyectos/01_texto_dinamico.py
+++ 01_Ejercicios/01_Nivel_Basico_flet/03_estilos_basicos/Proyectos/01_texto_dinamico.py
@@ -2,7 +2,7 @@ import flet as ft
 
 def main(page: ft.Page):
     # Configuración de la ventana
-    page.title = "Proyecto Integrador"
+    page.title = "Proyecto Integrador 01"
     page.bgcolor = "#FFF45"  # Fondo oscuro personalizado
 
     # Alineación global de la página
@@ -11,7 +11,7 @@ def main(page: ft.Page):
 
     # Texto centrado verticalmente
     titulo = ft.Text(
-        "Texto h1",
+        "Primer proyecto integrador: Creando un\ntexto con estilos básicos",
         color="white",
         size=32,
         weight="bold",
@@ -20,24 +20,31 @@ def main(page: ft.Page):
     #Espaciado 1
     esp_1 = ft.Text(
         " ",
-        size=24,
+        size=8,
     )
 
     # Texto alineado a la derecha
     subtitulo = ft.Text(
-        "Texto h2",
+        "Un ejercicio para practicar el manejo de texto y estilos \nen Flet",
         color="lightgray",
         size=23,
         weight="bold",
         italic=True
     )
 
+    #Espaciado 2
+    esp_2 = ft.Text(
+        " ",
+        size=12,
+        )
+
     # Texto en la parte inferior de la ventana
     parrafo = ft.Text(
-        "Texto en la parte inferior de la ventana",
+        "En esta unidad haz aprendido a manejar de forma general caracteristicas de texto\nadmás haz aprendido a darle estilos básicos como:\n\n 1) Cambiar el tamaño de la fuente\n 2) Modificar el color de la fuente\n 3) Dar a la fuente un formato de negritas",
         color="white",
         size=16,
     )
+
 
 
     # Agregar los textos en orden
@@ -45,6 +52,7 @@ def main(page: ft.Page):
         titulo,  
         esp_1,   # Texto alineado a la derecha (mantiene la alineación horizontal)
         subtitulo,  # Texto final que se ubica naturalmente en el flujo inferior
+        esp_2,
         parrafo
     )
 
